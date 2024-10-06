12449ebba616bf225a421b9910d80da6ad35fbe2
Dastan <astadavidov@gmail.com>
2024-10-06 18:32:04 +0600

    Slide SHow

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -7,17 +7,15 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_second);
     }
-    public void startNewActivity(View v){
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void goBack(View v){
+        Intent intent = new Intent(this, ThirdActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/ThirdActivity.java
+++ app/src/main/java/com/example/myapplication/ThirdActivity.java
@@ -1,23 +1,22 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.content.Intent;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_third);
+
 
     }
-    public void startNewActivity(View v){
-        Intent intent = new Intent(this, SecondActivity.class);
+    public void thirdActivity(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
