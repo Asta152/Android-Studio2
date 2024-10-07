2ff2c1ff079935b1cb1467c929a07a2fd23be9b8
Dastan <astadavidov@gmail.com>
2024-10-07 22:59:56 +0600

    Practice

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,10 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView Rating = findViewById(R.id.Rating);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfo("9.08 ot of 10");
+            }
+        });
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfo();
+            }
+        });
+
 
     }
     public void startNewActivity(View v){
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
+    private void showInfo(String text){
+
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+    }
+
 }
